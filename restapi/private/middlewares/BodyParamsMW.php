67fc9ed172fa2df6466ad3aced5fb1ad11fb4ed6
Dobó Márk <mark@marvinsystems.com>
2020-04-30 11:21:14 +0200

    fix

--- restapi/private/middlewares/BodyParamsMW.php
+++ restapi/private/middlewares/BodyParamsMW.php
@@ -14,7 +14,7 @@ class BodyParamsMW {
     public function __invoke(Request $request, RequestHandler $handler): Response {
         $keys = json_decode($request->getBody()->getContents(), true);
         // echo json_encode($this->depends['required_keys']);
-        if (Parameters::hasAllRequired($this->depends['required_keys'], $keys)){
+        if (is_array($keys) && Parameters::hasAllRequired($this->depends['required_keys'], $keys)){
             return $handler->handle($request);
         } else {
             $response = new \Slim\Psr7\Response();
